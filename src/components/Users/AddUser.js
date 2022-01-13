75c80e8266e5aff553eba119e2e810c878ce2aee
Vitaly Semenov <mtgmind@gmail.com>
2022-01-13 15:18:32 +0100

    Add ref

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,32 +1,27 @@
-import {useState} from "react";
+import {useRef} from "react";
 import Card from "../UI/Card";
 
 import classes from './AddUser.module.css';
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
-    const [enteredName, setEnteredName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
-
-    const nameChangeHandler = (event) => {
-        setEnteredName(event.target.value);
-    }
-
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    }
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
 
     const submitHandler = (event) => {
         event.preventDefault();
 
-        if (enteredName.trim().length === 0 || enteredAge.length === 0) {
+        const eName = nameInputRef.current.value;
+        const eAge = ageInputRef.current.value;
+
+        if (eName.trim().length === 0 || eAge.length === 0) {
             props.onShowModal(true);
             props.setModalHeader("Invalid input");
             props.setModalMessage("Please enter a valid name and age (non-empty values)");
             return;
         }
 
-        if (Number(enteredAge) <= 0) {// we can use +enteredAge to convert value to number
+        if (Number(eAge) <= 0) {// we can use +enteredAge to convert value to number
             props.onShowModal(true);
             props.setModalHeader("Invalid age");
             props.setModalMessage("Please enter a valid age (> 0)");
@@ -35,23 +30,23 @@ const AddUser = (props) => {
 
         const newUser = {
             id: Math.random(),
-            name: enteredName,
-            age: enteredAge,
+            name: eName,
+            age: eAge,
         };
 
         props.onAddUser(newUser);
 
-        setEnteredName('');
-        setEnteredAge('')
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
 
     return (
         <Card className={classes.input}>
             <form onSubmit={submitHandler}>
                 <label htmlFor="username">Name</label>
-                <input id="username" type="text" value={enteredName} onChange={nameChangeHandler}/>
+                <input id="username" type="text" ref={nameInputRef}/>
                 <label htmlFor="age">Age</label>
-                <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                <input id="age" type="number" ref={ageInputRef}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
